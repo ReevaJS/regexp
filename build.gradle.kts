13de806ad6126c10f521d91f219f81a5b5e31cfb
Matthew Olsson <matthewcolsson@gmail.com>
2022-09-16 17:02:36 -0700

    Build: Add publishing tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,15 @@
+import java.io.ByteArrayOutputStream
+import org.gradle.jvm.tasks.Jar
+
 plugins {
     kotlin("jvm") version "1.7.10"
+    `maven-publish`
     application
 }
+
+group = "com.reevajs"
+version = "1.0.0"
+val release = false
 
 repositories {
     mavenCentral()
@@ -9,7 +17,6 @@ repositories {
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.ibm.icu:icu4j:71.1")
 }
 
@@ -25,7 +32,7 @@ tasks {
     compileKotlin {
         kotlinOptions {
             jvmTarget = "1.8"
-            freeCompilerArgs = listOf("-Xinline-classes", "-Xopt-in=kotlin.RequiresOptIn")
+            freeCompilerArgs = listOf("-Xinline-classes", "-opt-in=kotlin.RequiresOptIn")
         }
     }
 
@@ -34,4 +41,31 @@ tasks {
             jvmTarget = "1.8"
         }
     }
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = if (release) project.version.toString() else getGitHash()
+
+            from(components["java"])
+            artifact(sourcesJar.get())
+        }
+    }
+}
+
+fun getGitHash(): String {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine("git", "rev-parse", "--short", "HEAD")
+        standardOutput = stdout
+    }
+    return stdout.toString().trim()
 }
